d2f6d724c87e9a7fb7cd275e027eafb0af7c55a9
Vicente <155541350+gonzalezz13@users.noreply.github.com>
2026-07-01 23:04:06 +0200

    Build de producción listo para deploy

--- peluqueria-youssef/src/app/app.ts
+++ peluqueria-youssef/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Index } from './index/index';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Index],
+  imports: [Navbar, Index, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
